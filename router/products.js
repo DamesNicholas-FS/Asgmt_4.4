8d5acb587d89bc61a2b214cb6242cc74f8daa043
Nicholas Dames <nicholasdames@Nicholass-iMac.local>
2022-10-16 14:08:55 -0400

    second init

--- router/products.js
+++ router/products.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { restart } = require("nodemon");
 const product = require("../api/models/product");
 const router = express.Router();
 
 //Get
-router.get('/',(req,res, next) => {
-    
-    product.find({}).then(result => {
-        console.log(result)
-        res.status(200).json({
-            message: 'View All Products',
-            product: {
-                name: result.name,
-                company: result.company,
-                id: result._id,
-                metadata: {
-                    method: req.method,
-                    host: req.hostname
+router.get('/',(req,res,next) => {
+    product.find({})
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: 'View All Products',
+                product: {
+                    name: result.name,
+                    company: result.company,
+                    id: result._id,
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname
+                    }
                 }
-            }
+            })
         })
-    })
-    .catch( err => {
-        console.error(err.message);
-        res.status(500).json({
-            error:{
-                message: err.message,
-            }
+        .catch( err => {
+            console.error(err.message);
+            res.status(500).json({
+                error:{
+                    message: err.message,
+                }
+            })
         })
-    })
 
     // res.json({
     //     message: "Products-GET"
@@ -80,29 +81,30 @@ router.post('/',(req,res,next) => {
 router.get('/:productId', (req,res,next) =>{
     const productId = req.params.productId;
     
-    product.findOne({productId}).then(result => {
-        console.log(result)
-        res.status(200).json({
-            message: 'Found: ' + result.name,
-            product: {
-                name: result.name,
-                company: result.company,
-                id: result._id,
-                metadata: {
-                    method: req.method,
-                    host: req.hostname
+    product.findOne({productId})
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: 'Found: ' + result.name,
+                product: {
+                    name: result.name,
+                    company: result.company,
+                    id: result._id,
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname
+                    }
                 }
-            }
+            })
         })
-    })
-    .catch( err => {
-        console.error(err.message);
-        res.status(500).json({
-            error:{
-                message: err.message,
-            }
+        .catch( err => {
+            console.error(err.message);
+            res.status(500).json({
+                error:{
+                    message: err.message
+                }
+            })
         })
-    })
 
     // res.json({
     //     message: "Products-GET ID",
@@ -154,10 +156,36 @@ router.patch('/:productId', (req,res,next) => {
 //Delete by ID - Delete
 router.delete('/:productId', (req,res,next) => {
     const productId = req.params.productId;
-    res.json({
-        message: 'Products-DELETE',
-        id: productId,
-    })
+
+    product.deleteOne({productId})
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: 'Item was Deleted Successful!',
+                product:{
+                    name: result.name,
+                    company: result.company,
+                    id: result._id,
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname
+                    }
+                }
+            })
+        })
+        .catch( err => {
+            console.error(err.message);
+            res.status(500).json({
+                error:{
+                    message: err.message
+                }
+            })
+        })
+
+    // res.json({
+    //     message: 'Products-DELETE',
+    //     id: productId,
+    // })
 })
 
 module.exports = router
