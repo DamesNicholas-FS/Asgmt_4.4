960e6315ae69604f53cc383266a629b8bbe7a532
Nicholas Dames <nicholasdames@Nicholass-iMac.local>
2022-10-16 21:58:22 -0400

    Updating Files, Finally Upload for 3.6

--- router/products.js
+++ router/products.js
@@ -27,6 +27,42 @@ router.get('/',(req,res,next) => {
     // })
 })
 
+//Get with ID - Read w/ID
+router.get('/:productId', (req,res,next) =>{
+    const productId = req.params.productId;
+    
+    product.findOne({productId})
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: 'Found Product',
+                // message: 'Found: ' + result.name,
+                product: {
+                    name: result.name,
+                    company: result.company,
+                    id: result._id,
+                    metadata: {
+                        method: req.method,
+                        host: req.hostname
+                    }
+                }
+            })
+        })
+        .catch( err => {
+            console.error(err.message);
+            res.status(500).json({
+                error:{
+                    message: err.message
+                }
+            })
+        })
+
+    // res.json({
+    //     message: "Products-GET ID",
+    //     id: productId,
+    // })
+})
+
 //Post - Write  w/ID
 router.post('/',(req,res,next) => {
     
@@ -65,42 +101,6 @@ router.post('/',(req,res,next) => {
     
     // res.json({
     //     message: "Products-POST"
-    // })
-})
-
-//Get with ID - Read w/ID
-router.get('/:productId', (req,res,next) =>{
-    const productId = req.params.productId;
-    
-    product.findOne({productId})
-        .then(result => {
-            console.log(result)
-            res.status(200).json({
-                message: 'Found Product',
-                // message: 'Found: ' + result.name,
-                product: {
-                    name: result.name,
-                    company: result.company,
-                    id: result._id,
-                    metadata: {
-                        method: req.method,
-                        host: req.hostname
-                    }
-                }
-            })
-        })
-        .catch( err => {
-            console.error(err.message);
-            res.status(500).json({
-                error:{
-                    message: err.message
-                }
-            })
-        })
-
-    // res.json({
-    //     message: "Products-GET ID",
-    //     id: productId,
     // })
 })
 
